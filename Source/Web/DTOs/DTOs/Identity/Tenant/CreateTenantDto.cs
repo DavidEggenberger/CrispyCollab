aecaa2db546213a542a8c8c6a7204e2469f19cf5
DavidEggenberger <david99.eggenberger@gmail.com>
2022-02-26 06:35:12 +0100

    GroupNavMenu

---
+++ Source/Web/DTOs/DTOs/Identity/Tenant/CreateTenantDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.DTOs.Identity.Tenant
+{
+    public class CreateTenantDto
+    {
+        public string Name { get; set; }
+        public string Base64Data { get; set; }
+    }
+}
