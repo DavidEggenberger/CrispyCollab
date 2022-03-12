8386412dbc444a653972dcbf8cd84847e3c635a6
DavidEggenberger <david99.eggenberger@gmail.com>
2022-03-12 18:41:39 +0100

    Domain Interfaces

--- Source/Domain/Interfaces/IEmailSender.cs
+++ Source/Domain/Interfaces/IEmailSender.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Infrastructure.EmailSender
+namespace Domain.Interfaces
 {
     public interface IEmailSender
     {
--- Source/Web/WebServer/SignalR/WebClientNotificationService.cs
+++ Source/Web/WebServer/SignalR/WebClientNotificationService.cs
@@ -1,8 +1,24 @@
 ﻿using Infrastructure.Identity.Interfaces;
+using System;
+using System.Threading.Tasks;
 
 namespace WebServer.Hubs
 {
     public class WebClientNotificationService : IWebClientNotificationService
     {
+        public Task NotifyAdminTeamMembers(Guid teamId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task NotifyAllTeamMembers(Guid teamId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task NotifyUser(Guid userId)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
