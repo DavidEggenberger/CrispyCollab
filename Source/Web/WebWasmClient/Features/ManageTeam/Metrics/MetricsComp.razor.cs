a6767302230df6bad1a9b64f0f4a6acbd65d9778
DavidEggenberger <david99.eggenberger@gmail.com>
2022-03-13 11:07:46 +0100

    Filter Notifications

---
+++ Source/Web/WebWasmClient/Features/ManageTeam/Metrics/MetricsComp.razor.cs
@@ -0,0 +1,10 @@
+﻿using Common.Identity.Team.AdminManagement;
+using Microsoft.AspNetCore.Components;
+
+namespace WebWasmClient.Features.ManageTeam.Metrics
+{
+    public partial class MetricsComp
+    {
+        [Parameter] public TeamMetricsDTO TeamMetrics { get; set; }
+    }
+}
--- Source/Web/WebWasmClient/Features/ManageTeam/Notifications/AdminNotificationsComp.razor.cs
+++ Source/Web/WebWasmClient/Features/ManageTeam/Notifications/AdminNotificationsComp.razor.cs
@@ -1,11 +1,54 @@
 ﻿using Common.Identity.Team.AdminManagement;
 using Microsoft.AspNetCore.Components;
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace WebWasmClient.Features.ManageTeam.Notifications
 {
     public partial class AdminNotificationsComp
     {
         [Parameter] public List<AdminNotificationDTO> AdminNotifications { get; set; }
+        private List<AdminNotificationDTO> sortedAdminNotifications;
+        private Dictionary<string, Action> dateFilterings;
+        private string typeSorter;
+        public string TypeSorter
+        {
+            get => typeSorter;
+            set 
+            { 
+                typeSorter = value;
+                if(value == "All")
+                {
+                    sortedAdminNotifications = AdminNotifications.OrderByDescending(x => x.CreatedAt).ToList();
+                }
+                else
+                {
+                    sortedAdminNotifications = sortedAdminNotifications.Where(x => x.Type == (AdminNotificationTypeDTO)Enum.Parse(typeof(AdminNotificationTypeDTO), value)).ToList();
+                }
+                StateHasChanged();
+            }
+        }
+        private string dateSorter;
+        public string DateSorter
+        {
+            get => dateSorter;
+            set 
+            { 
+                dateSorter = value;
+                dateFilterings[value]();
+                StateHasChanged();
+            }
+        }
+        protected override async Task OnInitializedAsync()
+        {
+            sortedAdminNotifications = AdminNotifications.OrderByDescending(x => x.CreatedAt).ToList();
+            dateFilterings = new Dictionary<string, Action>()
+            {
+                ["newest"] = () => { sortedAdminNotifications = sortedAdminNotifications.OrderByDescending(x => x.CreatedAt).ToList(); },
+                ["oldest"] = () => { sortedAdminNotifications = sortedAdminNotifications.OrderBy(x => x.CreatedAt).ToList(); },
+            };
+        }
     }
 }
