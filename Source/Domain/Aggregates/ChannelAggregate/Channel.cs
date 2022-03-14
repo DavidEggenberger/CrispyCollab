0c037326a13d08a5a0e40ab0506f0d7c537c93c8
DavidEggenberger <david99.eggenberger@gmail.com>
2022-03-14 19:20:27 +0100

    ChannelAggregate

---
+++ Source/Domain/Aggregates/ChannelAggregate/Channel.cs
@@ -0,0 +1,17 @@
+﻿using Domain.SharedKernel.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Aggregates.ChannelAggregate
+{
+    [AggregateRoot]
+    public class Channel
+    {
+        public Guid TeamId { get; set; }
+        public string Name { get; set; }
+        public List<Message> MyProperty { get; set; }
+    }
+}
